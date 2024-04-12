a9570bbd8a9a0add9249845d2bf2b342c27faefb
SJ23y <mcfolland@gmail.com>
2024-04-12 23:11:39 +0300

    Исправлно поле base в настройках vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/322851-six-cities-15/14',
+  base: '/SJ-six-cities-15',
   plugins: [react()],
   test: {
     globals: true,
